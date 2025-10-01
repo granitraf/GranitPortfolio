c62aacf636c95fd1307fdbc550ce3874df7f0096
granitraf <granitraf@gmail.com>
2025-09-30 21:04:45 -0400

    changed

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: "granit-portfolio",
+	title: "granit rrafshi - portfolio",
 	description: "granit-portfolio",
 };
 
